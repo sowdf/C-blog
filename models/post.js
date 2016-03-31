3049c06ac38cc7579caea61ce31c093193d251e1
sowdf <229555644@qq.com>
2016-03-31 23:00:05 +0800

    5

--- models/post.js
+++ models/post.js
@@ -140,4 +140,58 @@ Post.edit = function(name,day,title,callback){
             })
         })
     })
+    //更新一篇文章及其相关
+    Post.update = function(name,day,title,post,callback){
+        //打开数据库
+        mongodb.open(function(err,db){
+            if(err){
+                return callback(err);
+            }
+            db.collection('posts',function(err,collection){
+                if(err){
+                    mongodb.close();
+                    return callback(err);
+                }
+                //更新文章内容
+                collection.update({
+                    'name':name,
+                    'time.day':day,
+                    'title':title
+                },{
+                    $set:{post:post}
+                },function(err){
+                    mongodb.close();
+                    if(err){
+                        return callback(err);
+                    }
+                    callback(null);
+                })
+            })
+        })
+    }
+}
+//删除一篇文章
+Post.remove = function(name,day,title,callback){
+    mongodb.open(function(err,db){
+        if(err){
+            return callback(err);
+        }
+        db.collection('posts',function(err,collection){
+            if(err){
+                mongodb.close();
+                return callback(err);
+            }
+            collection.remove({
+                'name':name,
+                'time.day':day,
+                'title':title
+            },{w:1},function(err){
+                mongodb.close();
+                if(err){
+                    return callback(err);
+                }
+                callback(null);
+            })
+        })
+    })
 }
