22a1cfecd7f6138c9708b5b6be0c87c81dd20865
sowdf <229555644@qq.com>
2016-04-03 07:22:17 +0800

    标签

--- models/post.js
+++ models/post.js
@@ -1,8 +1,9 @@
 var mongodb = require('./db.js'),
     markdown = require('markdown').markdown;
-function Post(name,title,post){
+function Post(name,title,tags,post){
     this.name = name;
     this.title = title;
+    this.tags = tags;
     this.post = post;
 }
 
@@ -24,6 +25,7 @@ Post.prototype.save = function(callback){
         name : this.name,
         time : time,
         title : this.title,
+        tags : this.tags,
         post : this.post,
         comments:[]
     }
@@ -151,35 +153,35 @@ Post.edit = function(name,day,title,callback){
             })
         })
     })
-    //更新一篇文章及其相关
-    Post.update = function(name,day,title,post,callback){
-        //打开数据库
-        mongodb.open(function(err,db){
+}
+//更新一篇文章及其相关
+Post.update = function(name,day,title,post,callback){
+    //打开数据库
+    mongodb.open(function(err,db){
+        if(err){
+            return callback(err);
+        }
+        db.collection('posts',function(err,collection){
             if(err){
+                mongodb.close();
                 return callback(err);
             }
-            db.collection('posts',function(err,collection){
+            //更新文章内容
+            collection.update({
+                'name':name,
+                'time.day':day,
+                'title':title
+            },{
+                $set:{post:post}
+            },function(err){
+                mongodb.close();
                 if(err){
-                    mongodb.close();
                     return callback(err);
                 }
-                //更新文章内容
-                collection.update({
-                    'name':name,
-                    'time.day':day,
-                    'title':title
-                },{
-                    $set:{post:post}
-                },function(err){
-                    mongodb.close();
-                    if(err){
-                        return callback(err);
-                    }
-                    callback(null);
-                })
+                callback(null);
             })
         })
-    }
+    })
 }
 //删除一篇文章
 Post.remove = function(name,day,title,callback){
@@ -232,5 +234,52 @@ Post.getArchive = function(callback){
                 callback(null,docs);
             })
         })
+    });
+}
+Post.getTags = function(callback){
+    mongodb.open(function(err,db){
+        if(err){
+            return callback(err);
+        }
+        db.collection('posts',function(err,collection){
+            collection.distinct('tags',function(err,docs){
+                mongodb.close();
+                if(err){
+                    return callback(err);
+                }
+                return callback(err,docs);
+            })
+        })
+
     })
+}
+Post.getTag = function(tag,callback){
+    //打开数据库
+    mongodb.open(function(err,db){
+        if(err){
+            return callback(err);
+        }
+        db.collection('posts',function(err,collection){
+            if(err){
+                mongodb.close();
+                return callback(err);
+            }
+            collection.find({
+                tags : tag
+            },{
+                'name':1,
+                'time':1,
+                'title':1
+            }).sort({
+                time : -1
+            }).toArray(function(err,docs){
+                mongodb.close();
+                if(err){
+                    return callback(err);
+                }
+                callback(null,docs);
+            })
+        })
+    });
+
 }
--- routes/index.js
+++ routes/index.js
@@ -127,7 +127,8 @@ module.exports = function(app){
   app.get('/post',checkLogin);
   app.post('/post', function (req, res) {
     var currentUser = req.session.user,
-        post = new Post(currentUser.name,req.body.title,req.body.post);
+        tags = [req.body.tag1, req.body.tag2, req.body.tag3],
+        post = new Post(currentUser.name, req.body.title, tags, req.body.post);
     post.save(function(err){
       if(err){
         req.flash('error',err);
@@ -172,6 +173,36 @@ module.exports = function(app){
       })
     })
   });
+  app.get('/tags',function(req,res){
+    Post.getTags(function(err,posts){
+      if(err){
+        req.flash('error',err);
+        return res.redirect('/');
+      }
+      res.render('tags',{
+        title : '标签',
+        posts : posts,
+        user : req.session.user,
+        success : req.flash('success').toString(),
+        error : req.flash('error').toString()
+      })
+    });
+  });
+  app.get('/tags/:tag',function(req,res){
+    Post.getTag(req.params.tag,function(err,posts){
+      if(err){
+        req.flash('error',err);
+        return res.redirect('/');
+      }
+      res.render('tag',{
+        title : 'TAG' + req.params.tag,
+        posts : posts,
+        user : req.session.user,
+        success : req.flash('success').toString(),
+        error : req.flash('error').toString()
+      })
+    })
+  })
   app.get('/u/:name',function(req,res){
     User.get(req,params.name,function(err,user){
       if(!user){
